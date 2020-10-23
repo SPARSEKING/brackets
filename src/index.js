a7bb8be4e6821ec91a6c3c43caa83a2fd77852ef
Kirill <gavrilchikkirill6@gmail.com>
2020-10-23 20:31:39 +0300

    feat: added task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrBrackets = [];
+  let newStr = str;
+
+  for(const item of bracketsConfig){
+    arrBrackets.push(item[0] + item[1]);
+  }
+
+  for (let i = 0; i < str.length; i++){
+    for(let j = 0; j < arrBrackets.length; j ++){
+      newStr = newStr.replace(arrBrackets[j], '');
+  }
+  }
+  if(newStr.length == 0){
+    return true;
+  }else{
+    return false;
+  }
 }
